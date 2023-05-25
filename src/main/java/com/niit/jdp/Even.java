863a7ee9113ec773eb6784f34769bce3a2db9328
pranay <pranayanand61@gmail.com>
2023-05-25 06:17:35 +0530

    found out even numbers from 1 to 50

---
+++ src/main/java/com/niit/jdp/Even.java
@@ -0,0 +1,16 @@
+package com.niit.jdp;
+
+public class Even {
+    public static void main(String[] args){
+        for (int number = 1; number <= 50 ; number++) {
+            if ( number%2 == 0){
+                System.out.println(number);
+            }
+
+        }
+    }
+
+
+
+
+}
